bf9774c32f2ca9726e1e696b43ff32431640d981
thennek0 <kennethbecaro0@gmail.com>
2023-09-24 23:33:11 +0800

    add tenant-management style structure

--- src/components/TenantManagement.jsx
+++ src/components/TenantManagement.jsx
@@ -1,24 +1,37 @@
+import tenant from "/public/tenant-management/tenant.svg";
 export default function TenantManagement() {
     return (
         <section className="tenant-management">
             <aside>
                 <div className="tenant-list">
-                    <div>1.Some Name</div>
-                    <div>1.Some Name</div>
-                    <div>1.Some Name</div>
-                    <div>1.Some Name</div>
+                    <h3>
+                        Tenant List
+                    </h3>
+                    <div>
+                        1.Some Name
+                        <button>Edit</button>
+                    </div>
+                    <div>
+                        1.Some Name
+                        <button>Edit</button>
+                    </div>
+                    <div>
+                        1.Some Name
+                        <button>Edit</button>
+                    </div>
                 </div>
                 <button>Add New Tenant</button>
             </aside>
             <section className="tenant-info">
-                <img src="" alt="tenant-image" />
+                <img src={tenant} alt="tenant-image" />
                 <div className="tenant-details">
+                    <h3>Tenant Information</h3>
                     <div>Full Name:</div>
                     <div>ID Number:</div>
                     <div>Phone Number</div>
                     <div>Assigned Room:</div>
                     <div>Room Type:</div>
-                    <div>Occupancy Status</div>
+                    <div>Occupancy Status:</div>
                 </div>
             </section>
             <section className="tenant-extra-info">
@@ -29,14 +42,126 @@ export default function TenantManagement() {
                     <option value="">Payment History</option>
                 </select>
                 <table>
-                    <tr>
-                        <th>some heading</th>
-                        <th>some heading</th>
-                    </tr>
-                    <tr>
-                        <td>some data</td>
-                        <td>some data</td>
-                    </tr>
+                    <thead>
+                        <tr>
+                            <th>some heading</th>
+                            <th>some heading</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                        <tr>
+                            <td>some data</td>
+                            <td>some data</td>
+                        </tr>
+                    </tbody>
                 </table>
             </section>
         </section>
