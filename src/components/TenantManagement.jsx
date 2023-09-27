a21e1fa5438ee7270f074bfba53203094547f8b6
thennek0 <kennethbecaro0@gmail.com>
2023-09-27 15:36:20 +0800

    add edit/create tenant pop-up and operation

--- src/components/TenantManagement.jsx
+++ src/components/TenantManagement.jsx
@@ -1,5 +1,6 @@
 import tenant from "/public/tenant-management/tenant.svg";
 import TableData from "./TableData";
+import TenantPopUp from "./TenantPopUp";
 import { useEffect, useState } from "react";
 export default function TenantManagement() {
     let [tenantList, setTenantList] = useState([]);
@@ -9,6 +10,19 @@ export default function TenantManagement() {
     let [tenantPaymentHistory, setTenantPaymentHistory] = useState([]);
     let [currentTenant, setCurrentTenant] = useState([]);
     let [isLoading, setIsLoading] = useState(true);
+    let [showPopUp, setShowPopUp] = useState(false);
+    let [popUpType, setPopUpType] = useState("");
+    let [selectedTenant, setSelectedTenant] = useState("");
+    function togglePopUp() {
+        if (showPopUp) {
+            setShowPopUp(false);
+        } else {
+            setShowPopUp(true);
+        }
+    }
+    function updateSelectedTenant(tenantObject) {
+        setSelectedTenant(tenantObject);
+    }
     useEffect(() => {
         fetch("http://localhost:3000/tenant", {
             method: "GET"
@@ -32,6 +46,7 @@ export default function TenantManagement() {
     }
     return (
         <section className="tenant-management">
+            <TenantPopUp updateSelectedTenant={updateSelectedTenant} selectedTenant={selectedTenant} showPopUp={showPopUp} togglePopUp={togglePopUp} popUpType={popUpType}></TenantPopUp>
             <aside>
                 <div className="tenant-list">
                     <h3>
@@ -39,15 +54,26 @@ export default function TenantManagement() {
                     </h3>
                     {
                         tenantList.map((element) => {
-                            return <div key={element.tenant_id}>{element.first_name + " " + element.last_name}<button>Edit</button></div>
+                            return <div key={element.tenant_id}>{element.first_name + " " + element.last_name}<button id={element.tenant_id} onClick={(e) => {
+                                setSelectedTenant({
+                                    tenantId: element.tenant_id,
+                                    firstName: element.first_name,
+                                    lastName: element.last_name,
+                                    contact: element.contact_number,
+                                    archiveStatus: element.archive_status,
+                                    identification: element.identification_number,
+                                });
+                                setPopUpType("edit");
+                                togglePopUp();
+                                console.log(selectedTenant)
+                            }}>Edit</button></div>
                         })
                     }
-                    {/* <div>
-                        1.Some Name
-                        <button>Edit</button>
-                    </div> */}
                 </div>
-                <button>Add New Tenant</button>
+                <button onClick={() => {
+                    setPopUpType("add");
+                    togglePopUp();
+                }}>Add New Tenant</button>
             </aside>
             <section className="tenant-info">
                 <img src={tenant} alt="tenant-image" />
