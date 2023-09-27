398387b209ab55f441c6318d2f9621b681ee7d54
thennek0 <kennethbecaro0@gmail.com>
2023-09-27 20:16:21 +0800

    add unpaid fees table selection

--- src/components/PaymentManagement.jsx
+++ src/components/PaymentManagement.jsx
@@ -1,22 +1,64 @@
+import { useState, useEffect } from "react";
+import TableData from "./TableData";
 export default function PaymentManagement() {
+    let [currentTable, setCurrentTable] = useState("room-table");
+    let [isLoading, setIsLoading] = useState(true);
+    let [tableData, setTableData] = useState([]);
+    // gets unpaid fees
+    useEffect(() => {
+        if (currentTable === "room-table") {
+            // room fees
+            fetch("http://localhost:3000/roomfee/unpaid", {
+                method: "GET"
+            }).then(response => {
+                return response.json();;
+            }).then((data) => {
+                // setRoomFees(data);
+                setTableData(data);
+                setIsLoading(false)
+            });
+        }
+        else if (currentTable === "utility-table") {
+            // utility fees
+            fetch("http://localhost:3000/utilityfee/unpaid", {
+                method: "GET"
+            }).then(response => {
+                return response.json();;
+            }).then((data) => {
+                // setUtilityFees(data);
+                setTableData(data);
+                setIsLoading(false)
+            });
+
+        }
+        else if (currentTable === "necessity-table") {
+            // necessity fees
+            fetch("http://localhost:3000/necessityfee/unpaid", {
+                method: "GET"
+            }).then(response => {
+                return response.json();;
+            }).then((data) => {
+                // setNecessityFees(data);
+                setTableData(data);
+                setIsLoading(false)
+            });
+        }
+    }, [currentTable]);
+    if (isLoading) {
+        return (<div>...loading</div>)
+    }
     return (
         <section className="payment-management">
             <div className="upcoming-dues">
-                <h3>Upcoming Dues</h3>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>some heading</th>
-                            <th>some heading</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>some data</td>
-                            <td>some data</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <select onChange={(e) => {
+                    setCurrentTable(e.target.value);
+                }} defaultValue="room-table" id="payment-type">
+                    <option value="room-table">Room</option>
+                    <option value="utility-table">Utility</option>
+                    <option value="necessity-table">Necessity</option>
+                </select>
+                <h3>Unpaid Payments</h3>
+                <TableData tenantData={tableData}></TableData>
             </div>
             <div className="payment-analytics">
                 payment analytics
--- src/components/TableData.jsx
+++ src/components/TableData.jsx
@@ -1,26 +1,58 @@
-function TableRow({ tableData, tableHeadings }) {
+function TableRow({ dataID, tableData, tableHeadings }) {
     return (
         <tr>
-            {tableHeadings.map(element => {
-                return <td>{tableData[element]}</td>
+            {tableHeadings.map((element, index) => {
+                const regex = /\d{4}-\d{2}-\d{2}/g;
+                if (typeof (tableData[element]) === "string") {
+                    const found = tableData[element].match(regex);
+                    let isFound = (found) ? true : false;
+                    if (isFound) {
+                        return <td key={tableData[dataID]}>{found[0]}</td>
+
+                    }
+                }
+                return <td key={index}>{tableData[element]}</td>
             })}
+            <td>
+                <button onClick={() => {
+                    console.log(tableData[dataID])
+                }}>details</button>
+            </td>
         </tr>
     )
 }
 export default function TableData({ tenantData }) {
-    let tableHeadings = Object.keys(tenantData[0]);
+    let dataKeys = Object.keys(tenantData[0]);
+    let tableHeadings = dataKeys.filter((element) => {
+        const regex = new RegExp('.*_id');
+        if (!(regex.test(element))) {
+            return true;
+        }
+    })
+    let tableId = dataKeys.filter((element) => {
+        const y = new RegExp('.*_id');
+        const x = new RegExp('^(?!.*tenant).*_id$');
+        if ((y.test(element))) {
+            if ((x.test(element))) {
+                return true;
+            }
+        }
+    })
     return (
         <table>
             <thead>
                 <tr>
-                    {tableHeadings.map((element) => {
-                        return <th>{element}</th>
-                    })}
+                    {
+                        tableHeadings.map((element) => {
+                            return <th>{element}</th>
+                        })
+                    }
+                    <th>Actions</th>
                 </tr>
             </thead>
             <tbody>
                 {tenantData.map((element, index) => {
-                    return <TableRow tableHeadings={Object.keys(element)} tableData={element}></TableRow>
+                    return <TableRow dataID={tableId[0]} tableHeadings={tableHeadings} tableData={element}></TableRow>
                 })}
             </tbody>
         </table>
