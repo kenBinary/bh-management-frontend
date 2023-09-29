0f6666e1d04d254c07a20845f15420dda85fa1a6
thennek0 <kennethbecaro0@gmail.com>
2023-09-29 21:59:06 +0800

    add dashboard stat summary

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,46 +1,162 @@
-// import { PieChart } from 'recharts';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { useState, useEffect } from "react";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, Cell, Label } from 'recharts';
 
 
-const SimpleBarChart = ({ data }) => {
+const SimpleLineChart = ({ data }) => {
     return (
-      <BarChart width={400} height={300} data={data}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="value" fill="#8884d8" />
-      </BarChart>
+        <ResponsiveContainer width="100%" height="100%">
+            <LineChart data={data}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="value" stroke="#8884d8" activeDot={{ r: 8 }} />
+            </LineChart>
+        </ResponsiveContainer>
+
     );
-  };
+};
+function SimplePieChart({ data }) {
+    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+    return (
+        <ResponsiveContainer width="100%" height="100%">
+            <PieChart>
+                <Pie
+                    data={data}
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={150}
+                    fill="#8884d8"
+                    label
+                >
+                    {data.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                </Pie>
+                <Tooltip />
+                <Legend />
+            </PieChart>
+        </ResponsiveContainer >
+    );
+};
+
 export default function Dashboard() {
-    const barChartData = [
+    let [didMount, setDidMount] = useState(false);
+    let [monthlyRevenue, setMonthlyRevenue] = useState(0);
+    let [totalTenants, setTotalTenants] = useState([]);
+    let [vacantRooms, setVacantRooms] = useState(0)
+    let [rentCollection, setRentCollection] = useState(0)
+    const lineChartData = [
+        { name: 'Jan', value: 25 },
+        { name: 'Feb', value: 30 },
+        { name: 'Mar', value: 45 },
+        { name: 'Apr', value: 28 },
+        { name: 'May', value: 35 },
+    ];
+    const pieChartData = [
         { name: 'Category A', value: 25 },
         { name: 'Category B', value: 30 },
         { name: 'Category C', value: 45 },
-      ];
+    ];
+    // initial render
+    useEffect(() => {
+        // get monthly revenue
+        fetch("http://localhost:3000/dashboard/monthly-revenue", {
+            method: "GET"
+        }).then((response) => {
+            return response.json();
+        }).then((data) => {
+            setMonthlyRevenue(data);
+        });
+
+        fetch("http://localhost:3000/dashboard/total-tenants", {
+            method: "GET"
+        }).then((response) => {
+            return response.json();
+        }).then((data) => {
+            setTotalTenants(data);
+        });
+        fetch("http://localhost:3000/dashboard/vacant-rooms", {
+            method: "GET"
+        }).then((response) => {
+            return response.json();
+        }).then((data) => {
+            setVacantRooms(data);
+        });
+        fetch("http://localhost:3000/dashboard/rent-collection", {
+            method: "GET"
+        }).then((response) => {
+            return response.json();
+        }).then((data) => {
+            console.log(data)
+            setRentCollection(data);
+        });
+        setDidMount(true);
+    }, [])
+    if (!didMount) {
+        return <div>Loading...</div>
+    }
     return (
         <section className="dashboard">
+            <div>
+                <h2>Dashboard</h2>
+            </div>
             <div className="stat-summary">
-                <div>Previous Month Revenue:</div>
-                <div>Number of Tenants:</div>
-                <div>Rent Collections:</div>
-                <div>Vacant Rooms:</div>
+                <div>
+                    <div>
+                        Monthly Revenue
+                    </div>
+                    <div>
+                        ₱ {monthlyRevenue.revenue}
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        Total Tenants
+                    </div>
+                    <div>
+                        {totalTenants.total_tenants}
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        Rent Collections
+                    </div>
+                    <div>
+                        {rentCollection.total}
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        Vacant Rooms:
+                    </div>
+                    <div>
+                        {vacantRooms.total_vacant}
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                </div>
             </div>
             <div className="tenant-room-analytics">
-                <table>
-                    <tr>
-                        <th>some heading</th>
-                        <th>some heading</th>
-                    </tr>
-                    <tr>
-                        <td>some data</td>
-                        <td>some data</td>
-                    </tr>
-                </table>
+                <div className="line-chart">
+                    <SimpleLineChart data={lineChartData}></SimpleLineChart>
+                </div>
                 <div className="occupancy-chart">
-                    <SimpleBarChart data={barChartData}></SimpleBarChart>
+                    <SimplePieChart data={pieChartData}></SimplePieChart>
                 </div>
             </div>
         </section>
--- src/components/RoomManagement.jsx
+++ src/components/RoomManagement.jsx
@@ -3,11 +3,52 @@ import doubleBed from "/room-management/double-bed.png"
 import RoomCard from "./RoomCard";
 import RoomPopUp from "./RoomPopUp";
 import { useEffect, useState } from "react";
+import { PieChart, Pie, Cell, Legend, Tooltip, ResponsiveContainer } from 'recharts';
+const SimplePieChart = ({ data }) => {
+    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042']; // Customize your pie chart colors
+    const RADIAN = Math.PI / 180;
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+        return (
+            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+                {`${(percent * 100).toFixed(0)}%`}
+            </text>
+        );
+    };
+    return (
+        <ResponsiveContainer width="100%" height="100%">
+            <PieChart>
+                <Pie
+                    data={data}
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                    fill="#8884d8"
+                    labelLine={false}
+                    label={renderCustomizedLabel}
+                    legendType="diamond"
+                >
+                    {data.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                </Pie>
+                <Tooltip />
+                <Legend />
+            </PieChart>
+        </ResponsiveContainer>
+    );
+};
 function RoomManagement() {
+    let [didMount, setDidMount] = useState(false);
     let [roomList, setRoomList] = useState([]);
     let [selectedRoom, setSelectedRoom] = useState({});
     let [showPopUp, setShowPopUp] = useState(false);
     let [popUpType, setPopUpType] = useState("assign");
+    let [roomStatusData, setRoomStatusData] = useState([]);
     const togglePopUp = () => {
         setShowPopUp((showPopUp) ? false : true);
     }
@@ -31,6 +72,20 @@ function RoomManagement() {
             roomPrice: cRoomPrice
         })
     }
+    // function filterRoom(filterTypes) {
+
+    // }
+    // initial render
+    useEffect(() => {
+        // get room status analytics
+        fetch("http://localhost:3000/room/status-analytics", {
+            method: "GET"
+        }).then((response) => {
+            return response.json();
+        }).then((data) => {
+            setRoomStatusData(data);
+        });
+    }, []);
     // get list of rooms
     useEffect(() => {
         fetch("http://localhost:3000/room", {
@@ -41,37 +96,39 @@ function RoomManagement() {
             setRoomList(data);
         });
     }, [showPopUp]);
-    // 
     return (
         <section className="room-management">
             <div className="room-filters">
                 <h3>Filter The List</h3>
                 <h5>Room Status</h5>
                 <div className="filter-option">
-                    <input type="radio" name="room-status" id="" />
-                    <label htmlFor="">Occupied</label>
+                    {/* roomList */}
+                    <input type="radio" name="status" id="occupied" value="occupied" />
+                    <label htmlFor="occupied">Occupied</label>
                 </div>
                 <div className="filter-option">
-                    <input type="radio" name="room-status" id="" />
-                    <label htmlFor="">Vacant</label>
+                    <input type="radio" name="status" id="vacant" value="vacant" />
+                    <label htmlFor="vacant">Vacant</label>
                 </div>
                 <h5>Room Price</h5>
                 <div className="filter-option">
-                    <input type="radio" name="room-price" id="" />
-                    <label htmlFor="">2500.00 php</label>
+                    <input type="radio" name="price" id="single-price" value="single-price" />
+                    <label htmlFor="single-price">2500.00 php</label>
                 </div>
                 <div className="filter-option">
-                    <input type="radio" name="room-price" id="" />
-                    <label htmlFor="">3500.00 php</label>
+                    <input type="radio" name="price" id="double-price" value="double-price" />
+                    <label htmlFor="double-price">3500.00 php</label>
                 </div>
                 <h5>Room Type</h5>
                 <div className="filter-option">
-                    <input type="radio" name="room-type" id="" />
-                    <label htmlFor="">Single Room</label>
+                    <input type="radio" name="type" id="single-type" value="single-type" />
+                    <label htmlFor="single-type">Single Room</label>
                 </div>
                 <div className="filter-option">
-                    <input type="radio" name="room-type" id="" />
-                    <label htmlFor="">Double Room</label>
+                    <input onChange={(e) => {
+                        console.log("buh");
+                    }} type="radio" name="type" id="double-type" value="double-type" />
+                    <label htmlFor="double-type">Double Room</label>
                 </div>
             </div>
             <div className="room-list">
@@ -86,7 +143,9 @@ function RoomManagement() {
                 </div>
             </div>
             <div className="room-analytics">
-                room analytics
+                <div className="room-status">
+                    <SimplePieChart data={roomStatusData}></SimplePieChart>
+                </div>
             </div>
         </section>
     )
